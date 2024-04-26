9c3f9048e5e02076a035fd3a4905afac976924cf
kyuuss <adam.czogallik@pm.me>
2024-04-26 11:41:07 +0200

    chore: updates

--- init.lua
+++ init.lua
@@ -257,6 +257,8 @@ require('lazy').setup({
   },
   { "nvim-neotest/nvim-nio" },
 
+  { "nvim-neotest/nvim-nio" },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
